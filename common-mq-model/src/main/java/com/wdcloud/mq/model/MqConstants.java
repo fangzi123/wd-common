034dc96034e37883ae636e9355388096c4895924
zhaoxiufei@gmail.com <zhaoxiufei@gmail.com>
2018-11-20 16:33:03 +0800

    add model

---
+++ common-mq-model/src/main/java/com/wdcloud/mq/model/MqConstants.java
@@ -0,0 +1,9 @@
+package com.wdcloud.mq.model;
+
+
+public class MqConstants {
+    public static final String QUEUE_OSS_CONVERT = "queue.oss.convert";
+    public static final String TOPIC_EXCHANGE_OSS_CONVERT = "topic.exchange.oss.convert";
+    public static final String QUEUE_LMS_OSS_CONVERT_RESULT = "queue.lms.convert.result";
+    public static final String TOPIC_EXCHANGE_LMS = "topic.exchange.lms";
+}
