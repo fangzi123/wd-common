9b03a569d920dd7365d82e112359eea322e8ff2f
zz <zhangzhen2540@qq.com>
2018-12-13 19:47:07 +0800

    add MessageUtil

---
+++ server-frame/src/main/java/com/wdcloud/server/frame/MessageUtil.java
@@ -0,0 +1,36 @@
+package com.wdcloud.server.frame;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.MessageSource;
+import org.springframework.context.NoSuchMessageException;
+import org.springframework.context.i18n.LocaleContextHolder;
+
+import javax.annotation.Resource;
+import java.util.Locale;
+
+@Slf4j
+public class MessageUtil {
+    private static MessageSource messageSource;
+
+    @Resource
+    public void setMessageSource(MessageSource messageSource) {
+        MessageUtil.messageSource = messageSource;
+    }
+
+    public String getMessage(String key) {
+        return getMessage(key, LocaleContextHolder.getLocale());
+    }
+
+    public String getMessage(String key, Locale locale) {
+        return getMessage(key, locale);
+    }
+
+    public String getMessage(String key, Locale locale, String... args) {
+        try {
+            return messageSource.getMessage(key, args, locale);
+        } catch (NoSuchMessageException e) {
+            log.warn("[LocalMessageSourceBean] can't find message for code '{}' at locale {}", key, LocaleContextHolder.getLocale());
+            return key;
+        }
+    }
+}
